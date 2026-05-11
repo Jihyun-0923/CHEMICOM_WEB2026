473d0fab4f276906cfd531bbd474f2f720d26086
Hawon <20252184@sungshin.ac.kr>
2026-05-11 16:36:53 +0900

    DB 조회 연결

--- compounds/views.py
+++ compounds/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
+from .models import Compound
+
 
 def home(request):
     return render(request, 'home.html')
 
+
 def result(request):
-    query = request.GET.get('query')
+    query = request.GET.get('query', '').strip()
+
+    compound = None
+    message = ''
+
+    if query:
+        compound = Compound.objects.filter(formula__iexact=query).first()
+
+        if not compound:
+            message = '검색 결과가 없습니다.'
+    else:
+        message = '화학식을 입력해주세요.'
 
     return render(request, 'result.html', {
-        'query': query
+        'query': query,
+        'compound': compound,
+        'message': message,
     })
-
 # Create your views here.
