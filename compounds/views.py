59b50d64f81c36797da513140fea9670934ee140
Jihyun <jessica092366@naver.com>
2026-05-12 00:31:03 +0900

    DB 초기 원소·화합물 데이터 반영

--- compounds/views.py
+++ compounds/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Q
 from django.shortcuts import render
-from .models import Compound
+
+from .models import Compound, Element
 
 
 def home(request):
@@ -10,19 +12,31 @@ def result(request):
     query = request.GET.get('query', '').strip()
 
     compound = None
+    element = None
     message = ''
 
     if query:
-        compound = Compound.objects.filter(formula__iexact=query).first()
+        compound = (
+            Compound.objects
+            .filter(Q(formula__iexact=query) | Q(name__icontains=query))
+            .first()
+        )
 
         if not compound:
+            element = (
+                Element.objects
+                .filter(Q(symbol__iexact=query) | Q(name__icontains=query))
+                .first()
+            )
+
+        if not compound and not element:
             message = '검색 결과가 없습니다.'
     else:
-        message = '화학식을 입력해주세요.'
+        message = '화합물명, 화학식, 원소명 또는 원소 기호를 입력해주세요.'
 
     return render(request, 'result.html', {
         'query': query,
         'compound': compound,
+        'element': element,
         'message': message,
     })
-# Create your views here.
